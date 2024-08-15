f4f4e91ba608898aad1c940b8ad125930d3007ae
vanshikasingh25 <vanshikasingh8860@gmail.com>
2024-08-15 21:45:29 +0530

    Add files via upload

---
+++ Task - Portfolio/script.js
@@ -0,0 +1,26 @@
+const slides = document.querySelectorAll(".slide")
+var counter=0;
+
+slides.forEach(
+    (slide,index) => {
+        slide.style.left ='${index*100}%'
+    }
+)
+
+const goprev = () =>{
+    counter--;
+    slideimage()
+}
+
+const gonext = () =>{
+    counter++;
+    slideimage()
+}
+
+const slideimage = () => {
+    slides.forEach(
+        (slide) => {
+            slide.style.transform = 'translateX(-${counter*100}%)'
+        }
+    )
+}
